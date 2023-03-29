221fb63fbf865888e31cb5cfe3eacf8d116c942b
purupuruputtyan <ec2-user@ip-172-31-7-79.ap-northeast-1.compute.internal>
2023-03-29 12:03:57 +0000

    [Finish]JavaScript-10

---
+++ js/script.js
@@ -0,0 +1,8 @@
+/*global $*/
+$(function() {
+  $('.menu-trigger').on('click', function(event) {
+    $(this).toggleClass('active');
+    $('#sp-menu').fadeToggle();
+    event.preventDefault();
+  });
+});
